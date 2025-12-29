ccc9425bb1527481d37f719c7a97e1f9e2c0d01d
Camberden <42500247+Camberden@users.noreply.github.com>
2025-12-28 22:41:34 -0500

    Further flex framing at index

--- action.js
+++ action.js
@@ -3,3 +3,15 @@ document.getElementById("string-thing").innerHTML = boosh;
 const date = new Date();
 document.querySelector("#latest-update").innerHTML = date.getMonth();
 
+// Successfully targets as inline =====>
+const paths = document.querySelectorAll("path");
+for (let path of paths) {
+	console.log(path);
+	const pathInitialColor = path.getAttribute("fill");
+	path.onmouseenter = () => {
+		path.style.fill = "magenta";
+	}
+	path.onmouseleave = () => {
+		path.style.fill = pathInitialColor;
+	}
+}
